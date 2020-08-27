70d993a6ccd4ccd8ac35e4e32ba89cff75f377ad
Subbaraman Narayanamurthy <subbaram@codeaurora.org>
2020-08-26 21:51:48 -0700

    diting: Add QTI health implementation
    Currently, this only has qti_healthd_board_init() that waits for
    battery power supply for a max timeout of 5 seconds. This is to
    ensure by the time health HAL service starts, battery power
    supply device is available. Without this, health HAL couldn't
    find battery power supply sometimes because of the time taken
    for battery charger driver to probe on platforms where it can
    happen only after remote subsytem is bought out of reset.
    
    Change-Id: I96ee53ef15dd61c98d317e9ec221456ca564249f
    
    healthd-ext: Ignore UCSI power supply name
    
    With the latest UCSI changes starting from Linux kernel 5.8+,
    ucsi-source-psy* is created per connector. This would interfere
    with the parameters published under "usb" power supply. Hence,
    add it to the ignored list of power supply names so that healthd
    battery monitor would skip monitoring it.
    
    Change-Id: I485c89a46b11714931f74adebd1bda8d0abd0ff3
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -491,8 +491,6 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
     android.hardware.sensors@2.1 \
     android.hardware.sensors@2.0-ScopedWakelock \
     android.hardware.sensors@2.0-ScopedWakelock.vendor \
