61b25bb49407f3c64bc0cb20688d84e182e76fee
Shaleen Agrawal <shalagra@quicinc.com>
2021-02-22 09:26:44 -0800

    diting: Enable Pelt8 during boot
    The default PELT half-life is 32ms. Change it to 8ms.
    
    Change-Id: I20b0f2209ebae45ef544cd2ba11ce13221dbc971
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -119,7 +119,8 @@ BOARD_KERNEL_CMDLINE := \
     swinfo.fingerprint=$(LINEAGE_VERSION) \
     mtdoops.fingerprint=$(LINEAGE_VERSION) \
     allow_file_spec_access \
-    irqaffinity=0-3
+    irqaffinity=0-3 \
+    pelt=8
 
 BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
