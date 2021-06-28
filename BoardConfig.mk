a55d8b6a924e6efe202eec69cebfe916085e47f2
Vijayanand Jitta <vjitta@qti.qualcomm.com>
2021-06-28 11:45:00 +0530

    diting: Enable allow_file_spec_access on waipio
    Allow speculative page faults on file backed pages
    by setting allow_file_spec_access on waipio.
    
    Change-Id: I970f2b85d000adb3b779a00833732d73f7932d79
    Signed-off-by: klozz <carlosj@klozz.dev>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -117,7 +117,8 @@ BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000 \
     disable_dma32=on \
     swinfo.fingerprint=$(LINEAGE_VERSION) \
-    mtdoops.fingerprint=$(LINEAGE_VERSION)
+    mtdoops.fingerprint=$(LINEAGE_VERSION) \
+    allow_file_spec_access
 
 BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
