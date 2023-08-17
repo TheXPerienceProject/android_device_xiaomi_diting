28a41077956d1fc430b09ec02bbcf25671c67371
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-08-17 19:27:58 +0530

    diting: health: Fix charge_counter and ETA values
    Stock kernel reports these measurements in units that are incompatible
    with AOSP (courtesy of ximi), causing issues like incorrect battery
    usage stats and wrong ETA of charging completion on lockscreen. Adjust
    the values in health HAL before reporting them to framework.
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -250,7 +250,6 @@ PRODUCT_COPY_FILES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl-diting \
-    android.hardware.health@2.1-impl-diting.recovery \
     android.hardware.health@2.1-service
 
 # HIDL
