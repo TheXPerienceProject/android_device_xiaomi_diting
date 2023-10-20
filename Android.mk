5ef7a3267b931a2cac4ba196e0d7f3f1ff14ff86
klozz <carlosj@klozz.dev>
2023-10-20 05:38:41 +0000

    mondrian: add EGL symlinks as per stock

--- Android.mk
+++ Android.mk
@@ -218,6 +218,22 @@ ALL_DEFAULT_INSTALLED_MODULES += \
     $(FIRMWARE_WLAN_QCA_CLD_SYMLINKS) \
     $(FIRMWARE_WLAN_QCA_CLD_QCA6490_SYMLINKS)
 
+EGL_LIB_SYMLINKS := $(TARGET_OUT_VENDOR)/lib
+$(EGL_LIB_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "EGL lib symlinks: $@"
+	@mkdir -p $@
+	$(hide) ln -sf egl/libEGL_adreno.so $@/libEGL_adreno.so
+	$(hide) ln -sf egl/libGLESv2_adreno.so $@/libGLESv2_adreno.so
+	$(hide) ln -sf egl/libq3dtools_adreno.so $@/libq3dtools_adreno.so
+
+EGL_LIB64_SYMLINKS := $(TARGET_OUT_VENDOR)/lib64
+$(EGL_LIB64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "EGL lib64 symlinks: $@"
+	@mkdir -p $@
+	$(hide) ln -sf egl/libEGL_adreno.so $@/libEGL_adreno.so
+	$(hide) ln -sf egl/libGLESv2_adreno.so $@/libGLESv2_adreno.so
+	$(hide) ln -sf egl/libq3dtools_adreno.so $@/libq3dtools_adreno.so
+
 IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
 IMS_SYMLINKS := $(addprefix $(TARGET_OUT_SYSTEM_EXT_APPS_PRIVILEGED)/Ims/lib/arm64/,$(notdir $(IMS_LIBS)))
 $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
@@ -227,7 +243,9 @@ $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	$(hide) ln -sf /system_ext/lib64/$(notdir $@) $@
 
 ALL_DEFAULT_INSTALLED_MODULES += \
-	$(IMS_SYMLINKS)
+	$(IMS_SYMLINKS) \
+	$(EGL_LIB_SYMLINKS) \
+	$(EGL_LIB64_SYMLINKS)
 
 FIRMWARE_SYMLINKS := $(TARGET_OUT_VENDOR)/firmware/
 $(FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
