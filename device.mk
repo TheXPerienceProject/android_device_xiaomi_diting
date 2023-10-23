78e26baf4fb705a194cb64f9a5ecf2b581e13eb8
klozz <carlosj@klozz.dev>
2023-10-23 03:48:50 -0600

    mondrian: add udfps support
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -172,10 +172,12 @@ PRODUCT_PACKAGES += \
     fastbootd
 
 # Fingerprint
-#PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    libudfpshandler \
+    sensors.xiaomi
 
-#PRODUCT_COPY_FILES += \
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 # Fastbootd
