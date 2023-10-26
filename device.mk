a53fc5f5871cb0b4e8fc96364d9f574c54c17321
klozz <carlosj@klozz.dev>
2023-10-25 21:41:04 -0600

    diting: include miui camera if exist
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -154,6 +154,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+#include miui
+ $(call inherit-product-if-exists, device/xiaomi/miuicamera-diting/device.mk)
+
 # Capabilityconfigstore
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.capabilityconfigstore@1.0.vendor
