479b6ff4612a4dee31f2f5eeff432a156cba4c89
klozz <carlosj@klozz.dev>
2023-10-29 23:50:11 -0600

    diting: bluetooth: Set Bluetooth name
    Signed-off-by: klozz <carlosj@klozz.dev>

--- libinit/libinit_variant.cpp
+++ libinit/libinit_variant.cpp
@@ -35,6 +35,7 @@ void set_variant_props(const variant_info_t variant) {
     set_ro_build_prop("model", variant.model, true);
     set_ro_build_prop("mod_device", variant.model, true);
     property_override("vendor.usb.product_string", variant.marketname, true);
+    property_override("bluetooth.device.default_name", variant.marketname, true);
     if (access("/system/bin/recovery", F_OK) != 0) {
         set_ro_build_prop("fingerprint", variant.build_fingerprint);
         property_override("ro.bootimage.build.fingerprint", variant.build_fingerprint);
