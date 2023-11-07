5a0c9e05e2b55716c9a8013fe5a7e3cb63238a77
klozz <carlosj@klozz.dev>
2023-11-07 00:51:16 -0600

    diting: initial derp a14 support
    Signed-off-by: klozz <carlosj@klozz.dev>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_diting.mk
+    $(LOCAL_DIR)/derp_diting.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_diting-eng \
-    lineage_diting-userdebug \
-    lineage_diting-user
+    derp_diting-eng \
+    derp_diting-userdebug \
+    derp_diting-user
