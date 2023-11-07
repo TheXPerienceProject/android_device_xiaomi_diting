1a5ed9fd8b13e04ac9b4f12fa87f7571a1f00b9d
klozz <carlosj@klozz.dev>
2023-11-07 01:16:25 -0600

    diting: derpify device_manifest_xml
    Signed-off-by: klozz <carlosj@klozz.dev>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -260,7 +260,7 @@ DEVICE_MANIFEST_CAPE_FILES := \
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/vendor_framework_compatibility_matrix.xml \
     $(DEVICE_PATH)/configs/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/derp/config/device_framework_matrix.xml
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
