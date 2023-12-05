86d44a747ec388948ccbc5b46c512b40d1f95e2f
klozz <carlosj@klozz.dev>
2023-12-05 02:50:27 -0600

    [Revertme]diting: downgrade media to 1.0 instead of 1.2
     * just for testing
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -354,11 +354,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.media.c2@1.2.vendor \
+    android.hardware.media.c2@1.0.vendor \
     libavservices_minijail \
     libavservices_minijail_vendor \
     libavservices_minijail.vendor \
-    libcodec2_hidl@1.2.vendor \
+    libcodec2_hidl@1.0.vendor \
     libcodec2_soft_common.vendor \
     libcodec2_vndk.vendor \
     libpalclient \
