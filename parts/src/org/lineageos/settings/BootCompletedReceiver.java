05f9e2c550a8e90c572fc3b1e123976056f0fc12
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-12-26 06:20:05 +0530

    diting: Update overrideHdrTypes API for U
    It's moved to DisplayManager in U.
    
    Change-Id: Ia5b5764a298c185cd5f25577881162e3bf2212d2
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -21,10 +21,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.hardware.display.DisplayManager;
 import android.util.Log;
 import android.os.IBinder;
+import android.view.Display;
 import android.view.Display.HdrCapabilities;
-import android.view.SurfaceControl;
 
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.dolby.DolbyUtils;
@@ -69,8 +70,8 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         AodBrightnessService.startService(context);
 
         // Override HDR types
-        final IBinder displayToken = SurfaceControl.getInternalDisplayToken();
-        SurfaceControl.overrideHdrTypes(displayToken, new int[]{
+        final DisplayManager displayManager = context.getSystemService(DisplayManager.class);
+        displayManager.overrideHdrTypes(Display.DEFAULT_DISPLAY, new int[]{
                 HdrCapabilities.HDR_TYPE_DOLBY_VISION, HdrCapabilities.HDR_TYPE_HDR10,
                 HdrCapabilities.HDR_TYPE_HLG, HdrCapabilities.HDR_TYPE_HDR10_PLUS});
     }
