321e4e1645cfac5b8207178247d3cfa9c086e330
klozz <carlosj@klozz.dev>
2024-02-15 22:45:53 -0600

    diting:remove non existent ldac codec
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -126,7 +126,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-V2-ndk.vendor \
     android.hardware.bluetooth.audio-impl \
     libldacBT_abr \
-    libldacBT_bco \
     libldacBT_enc \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
