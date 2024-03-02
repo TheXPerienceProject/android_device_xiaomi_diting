a3e66fa506d5532b010dd7d3acebd8247d5bf5d9
klozz <carlosj@klozz.dev>
2024-03-01 20:31:30 -0600

    diting: switch to ARM64 only
    Signed-off-by: klozz <carlosj@klozz.dev>

--- lineage_diting.mk
+++ lineage_diting.mk
@@ -5,7 +5,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
@@ -38,4 +38,4 @@ BUILD_GOOGLE_DIALER := true
 BUILD_GOOGLE_CONTACTS := true
 BUILD_GOOGLE_MESSAGE := true
 TARGET_HAS_UDFPS := true
-PRODUCT_USE_SCUDO := true
+#PRODUCT_USE_SCUDO := true
