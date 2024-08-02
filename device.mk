88fce5fcebc965675f54879f7a4d3be39075be94
Mohammad Nt <mohammadnt96@gmail.com>
2024-08-02 08:52:10 +0330

    diting: fix dolby by dolbycodec_shim

--- device.mk
+++ device.mk
@@ -392,7 +392,8 @@ PRODUCT_PACKAGES += \
     libcodec2_soft_common.vendor \
     libcodec2_vndk.vendor \
     libpalclient \
-    libsfplugin_ccodec_utils.vendor
+    libsfplugin_ccodec_utils.vendor \
+    dolbycodec_shim
 
 PRODUCT_PACKAGES += \
     init.qti.media.rc \
