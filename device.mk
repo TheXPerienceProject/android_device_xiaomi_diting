184017b743ba063d4e5c89065e5640a57ad0bfbe
Mohammad Nt <mohammadnt96@gmail.com>
2024-08-02 09:12:57 +0330

    diting: remove miuicamera

--- device.mk
+++ device.mk
@@ -173,8 +173,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
-# Camera
-$(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
 
 # Display
 PRODUCT_COPY_FILES += \
