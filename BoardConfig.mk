65f702621c4d723b8d30fe09e1720dc015530bbb
Mohammad Nt <mohammadnt96@gmail.com>
2024-08-02 09:19:24 +0330

    diting: remove IGNORE NEVERALLOWS and permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -312,6 +312,3 @@ PRODUCT_VENDOR_MOVE_ENABLED := true
 # Inherit from the proprietary version
 -include vendor/xiaomi/diting/BoardConfigVendor.mk
 
-SELINUX_IGNORE_NEVERALLOWS := true
-
-BOARD_BOOTCONFIG += androidboot.selinux=permissive
