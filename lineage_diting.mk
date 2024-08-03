c08b3203076e9e7e74812cdaca839004b880a721
klozz <carlosj@klozz.dev>
2024-08-03 18:25:20 +0000

    diting: rising OS bringup
    Signed-off-by: klozz <carlosj@klozz.dev>

--- lineage_diting.mk
+++ lineage_diting.mk
@@ -14,8 +14,6 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from diting device.
 $(call inherit-product, device/xiaomi/diting/device.mk)
 
-$(call inherit-product, vendor/gapps/common/common-vendor.mk)
-
 ## Device identifier
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := diting
@@ -26,20 +24,21 @@ PRODUCT_NAME := lineage_diting
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="diting_global-user 14 UKQ1.230917.001 V816.0.4.0.ULFMIXM release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
     PRODUCT_NAME=diting_global
 
-# Matrix
-MATRIXX_MAINTAINER := Kισżż
-MATRIXX_CHIPSET := SM8475
-MATRIXX_BATTERY := 5000mah
-MATRIXX_DISPLAY := 1220x2712
-WITH_GMS := true
-WITH_GAPPS := true
-EXTRA_GAPPS := true
-BUILD_GOOGLE_DIALER := true
-BUILD_GOOGLE_CONTACTS := true
-BUILD_GOOGLE_MESSAGE := true
-TARGET_HAS_UDFPS := true
-PRODUCT_USE_SCUDO := true
-# Blur
+BUILD_FINGERPRINT := Xiaomi/diting_global/diting:14/UKQ1.230917.001/V816.0.4.0.ULFMIXM:user/release-keys
+
+# RisingOS flags
+PRODUCT_NO_CAMERA := false
 TARGET_ENABLE_BLUR := true
+RISING_MAINTAINER := Kισżż
+RISING_CHIPSET="Snapdragon 8+ Gen1"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RISING_MAINTAINER="Kισżż" \
+    RISING_CHIPSET="Snapdragon 8+ Gen1"
+TARGET_BOOT_ANIMATION_RES := 1440
+WITH_GMS := true
+TARGET_HAS_UDFPS := true
