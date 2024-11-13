8cebf70fd5c4b6e0cc0a0219ee05e5b0776c58ee
klozz <carlosj@klozz.dev>
2024-11-12 18:09:59 -0600

    diting: build meme cam
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -141,6 +141,8 @@ PRODUCT_PACKAGES_DEBUG += \
     bootctl
 
 # Camera
+$(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
+
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.7.vendor \
     android.hardware.graphics.common-V4-ndk \
