c3a2856d35428d2128dd9bc4e102bfc3ddacb9e0
Jake Weinstein <jake@aospa.co>
2024-11-26 02:48:57 +0530

    diting: Dynamically set IRQ affinity for KGSL and  MSM_DRM
    - Introduce a new shell script to set IRQ affinity, since different
      kernels can have different IRQ numbers.
    
    Co-Authored-By: Alex Naidis <alex.naidis@paranoidandroid.co>
    Co-Authored-By: Jake Weinstein <jake@aospa.co>
    Change-Id: I61d2350e6b40f096714148c568489579bc4d2371
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -589,6 +589,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/bin/init.qcom.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.sh \
     $(LOCAL_PATH)/rootdir/bin/init.qti.kernel.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qti.kernel.sh \
     $(LOCAL_PATH)/rootdir/bin/init.qti.write.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qti.write.sh \
+    $(LOCAL_PATH)/rootdir/bin/init.xiaomi_sm8475.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.xiaomi_sm8475.sh \
     $(LOCAL_PATH)/rootdir/bin/vendor_modprobe.sh:$(TARGET_COPY_OUT_VENDOR)/bin/vendor_modprobe.sh \
     $(LOCAL_PATH)/rootdir/etc/init.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.rc \
     $(LOCAL_PATH)/rootdir/etc/init.qti.kernel.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qti.kernel.rc \
