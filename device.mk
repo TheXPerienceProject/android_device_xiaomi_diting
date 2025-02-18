088abc7dfeb6645eaee7543dc3fd631a4bc5f290
Arian <arian.kulmer@web.de>
2025-02-18 18:00:35 +0100

    diting: Generate qcrilNr.db during build
    Change-Id: I1f3425713b9b5b1cc7185bc94124b9f01efe3abb
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -771,6 +771,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     xiaomi-telephony-stub
 
+PRODUCT_PACKAGES += \
+    qcrilNrDb_vendor
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
