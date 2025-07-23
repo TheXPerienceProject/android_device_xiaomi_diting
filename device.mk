ff96ca181b47db79bf0f59dc7034f808ce80600c
klozz <carlosj@klozz.dev>
2025-07-23 17:24:34 -0600

    diting: fix bluetooth (remove bt audio aidl)
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -147,7 +147,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio-V2-ndk.vendor \
-    android.hardware.bluetooth.audio-impl:64 \
     libldacBT_abr \
     libldacBT_enc \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
