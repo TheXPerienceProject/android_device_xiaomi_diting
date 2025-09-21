29026229cbb2e8cfd19a85a255187b3196bd01a3
klozz <carlosj@klozz.dev>
2025-09-20 22:38:11 -0600

    diting: Rename to android.hardware.power.stats-service.xiaomi and build correct aidl
    add missing sepolicy
    
    Change-Id: Ic541ffe8f9117526a1b3ca7d932d3e9124f1487a
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -585,6 +585,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/taro/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# Power Stats
+PRODUCT_PACKAGES += \
+    android.hardware.power.stats-service.xiaomi \
+    android.hardware.power.stats-service.xiaomi.xml
+
 # Product
 PRODUCT_USE_SCUDO := true
 
