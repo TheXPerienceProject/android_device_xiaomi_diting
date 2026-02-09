ca9652c4645ada39e02436625de7c2aacb25e037
klozz <carlosj@klozz.dev>
2026-02-08 22:31:25 -0600

    diting: Switch to AIDL bluetooth audio
    Change-Id: Ib53a784630e76afcf49a09784abbf22b1f22ef13
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -145,6 +145,7 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.audio-impl:64 \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio-V2-ndk.vendor \
     libldacBT_abr \
