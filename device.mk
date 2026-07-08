f20c3bbfc06f4ceb81f4cf1800b38d2f20dd0af8
klozz <carlosj@klozz.dev>
2026-07-08 05:00:06 -0600

    diting: move to vendor.lineage.touch-service.xiaomi
    Change-Id: I69fb4077c1d9a8ca012e1dfb784b1d142498ba5f
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -805,8 +805,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+# Touchscreen
 PRODUCT_PACKAGES += \
-    vendor.xperience.touch@1.0-service.diting
+    vendor.lineage.touch-service.xiaomi
+
+$(call soong_config_set, XIAOMI_TOUCH, HIGH_TOUCH_POLLING_PATH, /sys/devices/virtual/touch/touch_dev/bump_sample_rate)
 
 # USB
 PRODUCT_HAS_GADGET_HAL := true
