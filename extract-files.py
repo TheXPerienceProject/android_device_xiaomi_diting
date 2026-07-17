48afcdab9b0b50c9ab546f3ea14113c6bd76cfe8
klozz <carlosj@klozz.dev>
2026-07-17 03:16:50 -0600

    diting: patch libwfdservice same as last commit
    Change-Id: I23e2ebe6f0154e481cc73f5383f1c60d01d4ffbd
    Signed-off-by: klozz <carlosj@klozz.dev>

--- extract-files.py
+++ extract-files.py
@@ -83,6 +83,7 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libbinder_shim.so')
         .add_needed('libinput_shim.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .add_needed('libaudiobase.so')
         .replace_needed(
             'android.media.audio.common.types-V4-cpp.so',
             'android.media.audio.common.types-V5-cpp.so',
