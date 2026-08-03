f4ff7d1aa29374e12ebc886899b1fc36d8582057
klozz <carlosj@klozz.dev>
2026-08-02 20:21:37 -0600

    diting: switch to qcom health AIDL
    Change-Id: I2facec59b5e0bc6760b2af423c319e8d04d235bc
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -353,8 +353,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl-diting \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery
 
 # HotwordEnrollement
 PRODUCT_COPY_FILES += \
